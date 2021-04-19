0ceb41c1c9c1471e0cc8500975c23873d91b98d5
Zsombor Lazar <lzs@melexis.com>
2021-04-19 14:35:21 +0200

    static removed

--- build.sh
+++ build.sh
@@ -11,6 +11,3 @@ emacs --batch --no-init-file --load ./projects.lisp --funcall toggle-debug-on-er
 html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ${PUBLIC_DIR} --output-dir ${PUBLIC_DIR} --file-ext html
 
 html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ${PUBLIC_DIR} --output-dir ${PUBLIC_DIR} --file-ext css
-
-mv ${GITHUB_WORKSPACE}/static/hugo/* ${PUBLIC_DIR}/
-mv ${GITHUB_WORKSPACE}/static/googlee20ada3c6370fb77.html ${PUBLIC_DIR}/
