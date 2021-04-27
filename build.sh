512136704e074e67e4875fca1f1420eebc79971e
Zsombor Lazar <lzs@melexis.com>
2021-04-27 14:23:33 +0200

    site verification added to public

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ emacs --batch --no-init-file --load ./projects.lisp --funcall toggle-debug-on-er
 html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ${PUBLIC_DIR} --output-dir ${PUBLIC_DIR} --file-ext html
 
 html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ${PUBLIC_DIR} --output-dir ${PUBLIC_DIR} --file-ext css
+
+mv ${GITHUB_WORKSPACE}/static/google20f584e42dbb947e.html ${PUBLIC_DIR}/
